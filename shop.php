5c7fafbaf19916629c727ff2f7b8f1101dd4b948
FrostLohe <44663580+FrostLohe@users.noreply.github.com>
2018-11-01 08:31:16 +0100

    Update shop.php

---
+++ shop.php
@@ -0,0 +1,13 @@
+<?php
+	try
+	{
+		$bdd = new PDO('mysql:host=127.0.0.1:49614;dbname=localdb;charset=utf8', 'azure', '6#vWHD_$');
+	}
+	catch(Exception $e)
+	{
+        die('Erreur : '.$e->getMessage());
+	}
+	
+	echo "qzshdfpgqçerhfpdmvuqhwsel";
+	
+?>
