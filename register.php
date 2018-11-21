56b4d86acda2990cf9d710326bddc1c93cd1449f
FrostLohe <44663580+FrostLohe@users.noreply.github.com>
2018-11-21 22:08:24 +0200

    Add files via upload

--- register.php
+++ register.php
@@ -3,7 +3,7 @@
 require_once "config.php";
  
 // Define variables and initialize with empty values
-$username = $password = $confirm_password = "";
+$username = $password = $confirm_password = $sponsor = "";
 $username_err = $password_err = $confirm_password_err = "";
  
 // Processing form data when form is submitted
@@ -60,9 +60,42 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $confirm_password_err = " Password did not match.";
         }
     }
+	
+	// Validate sponsor
+    if(empty(trim($_POST["sponsor"]))){
+        $sponsor_err = " Please enter a sponsor.";
+    } else{
+        // Prepare a select statement
+        $sql = "SELECT id FROM users WHERE username = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_sponsor);
+            
+            // Set parameters
+            $param_sponsor = trim($_POST["sponsor"]);
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                /* store result */
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) != 1){
+                    $sponsor_err = " This sponsor doesn't exist.";
+                } else{
+                    $sponsor = trim($_POST["sponsor"]);
+                }
+            } else{
+                echo " Oops! Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
     
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($sponsor_err)){
         
         // Prepare an insert statement
         $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
@@ -98,6 +131,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     &nbsp<title>Dungeons and Dragons Tool</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> 
 </head>
 <body>
         <h2 class="ml-5">Dungeons and Dragons Toolkit <small class="text-muted" >Log In & Register</small></h2>
@@ -164,16 +201,30 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 								<input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
 								<div class="invalid-feedbak"><small style="color:#FF0000";><?php echo $confirm_password_err; ?></small></div>
 							</div>
+							<div class="input-group mb-3 <?php echo (!empty($sponsor_err)) ? 'has-error' : ''; ?>">
+								<div class="input-group-prepend">
+									<span class="input-group-text" id="inputGroup-sizing-default">Sponsor</span>
+								</div>
+								<input type="text" name="sponsor" class="form-control" value="<?php echo $sponsor; ?>">
+								<div class="invalid-feedbak"><small style="color:#FF0000";><?php echo $sponsor_err; ?></small></div>
+							</div>
 							<div class="form-group">
 								<input type="submit" value="Sign up" class="btn btn-info ml-5">
 								<input type="reset" class="btn btn-light" value="Reset">
 							</div>
-							<p>Already have an account? <a href="" data-toggle="collapse" data-target="#collapse1" aria-expanded="false" aria-controls="collapse1">Log in here</a>.</p>
+							<p>Already have an account ? <a href="" data-toggle="collapse" data-target="#collapse1" aria-expanded="false" aria-controls="collapse1">Log in here</a>.</p>
+							<p>What's a sponsor ? <a href="#" class="text-info" data-toggle="tooltip" data-placement="top" title="A sponsor is the name of someone who already has an account.">Info</a></p>
 						</form>		
 					</div>
 				</div>
 			</div>
 		</div>
+	
+		<script>
+		$(document).ready(function(){
+		$('[data-toggle="tooltip"]').tooltip();   
+		});
+		</script>
 	
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
